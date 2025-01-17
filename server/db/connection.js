094023c6299a171fa643a28289a0457008982913
raihann1 <72947239+raihann1@users.noreply.github.com>
2025-01-16 22:03:16 -0500

    setup database

---
+++ server/db/connection.js
@@ -0,0 +1,26 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const uri = process.env.DB_URI || "";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+try {
+  // Connect the client to the server
+  await client.connect();
+  // Send a ping to confirm a successful connection
+  await client.db("admin").command({ ping: 1 });
+  console.log(
+   "MongoDB connection successful!"
+  );
+} catch(err) {
+  console.error(err);
+}
+
+let db = client.db("appointments");
+
+export default db;
